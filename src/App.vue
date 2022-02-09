2d73404f820989a3b97be1d8cd320c563d9c2ffa
abhishek-deshmukh <deshmukh.abhishek369@gmail.com>
2022-02-09 11:16:33 +0530

    basic setup complete

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'C G Lab NISER - Defeating Pain',
   meta: [
     { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'keywords', content: 'Pain, NISER, research, biology, defeating pain' },
   ],
 })
 </script>
--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,5 +1,6 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+  <main class="px-4 pb-10 pt-5 text-center text-gray-700 dark:text-gray-200">
+    <NavBar />
     <router-view />
     <Footer />
     <div class="mt-5 mx-auto text-center opacity-25 text-sm">
